6ca7961d7dbe34974effecf9eefef5deaf531819
Kristoffer Walker <kris@kixx.name>
2016-05-09 06:52:25 -0400

    Sass build
    	modified:   .gitignore
    	new file:   build.js
    	new file:   css/boilerplate.scss
    	new file:   css/helpers.scss
    	new file:   css/main.scss
    	new file:   css/normalize.scss
    	new file:   css/print.scss
    	new file:   css/site.scss
    	modified:   package.json
    	modified:   views/index.hbs

---
+++ build.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const treadmill = require('treadmill');
+const filepath = require('filepath');
+const sass = require('node-sass');
+
+const task = treadmill.task;
+
+const APPDIR = filepath.create(__dirname);
+
+task('css', (args, done) => {
+	const src = APPDIR.append('css', 'main.scss');
+	const dest = APPDIR.append('public', 'assets', 'css', 'main.css');
+
+	const options = {
+		file: src.toString(),
+		outputStyle: 'compressed' // nested, expanded, compact, compressed
+	};
+
+	sass.render(options, (err, res) => {
+		if (res) {
+			dest.write(res.css).then(() => {
+				done();
+			}, done);
+		} else {
+			done(err);
+		}
+	});
+});
