adeee9b6d2ed4b050c04313fb44db90953e87920
Kristoffer Walker <kris@kixx.name>
2016-06-27 20:29:10 -0400

    Adds Referer header to 404 warning log
    	modified:   middleware/not-found.js

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -4,7 +4,11 @@ module.exports = function (app) {
 	const log = app.API.log;
 
 	return function notFound(req, res) {
-		log.warn({path: req.path}, 'page not found');
+		log.warn(
+			{req: {path: req.path, ref: req.get('Referrer') || null}},
+			'page not found'
+		);
+
 		res
 			.status(404)
 			.render('404');
