c0c568e453ff8ac593b8bcb88f64ba78cabdd338
Kristoffer Walker <kris@kixx.name>
2016-06-30 16:37:12 -0400

    Remove deprecated AWS deploy stuff
    	deleted:    appspec.yml
    	deleted:    build/.gitkeep
    	deleted:    config/nginx/nginx.conf
    	deleted:    config/nginx/sites-available/default
    	deleted:    config/push-exclude.list
    	deleted:    deployment/AfterInstall.sh
    	deleted:    deployment/ApplicationStart.sh
    	deleted:    deployment/ApplicationStop.sh
    	deleted:    deployment/shared.sh
    	deleted:    tools/aws-instance-setup.sh
    	deleted:    tools/launch-ec-2-instances.js
    	deleted:    tools/machine-image-provision.sh

--- deployment/ApplicationStop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-THISDIR="$(cd `dirname "$0"` && pwd)"
-source "$THISDIR/shared.sh"
-
-su -c "'HOME=/home/ubuntu && pm2 stop $APPNAME'" $APPUSER
--- deployment/shared.sh
+++
@@ -1,3 +0,0 @@
-APPNAME="oddnetworks"
-APPDIR=/var/www/oddnetworks/
-APPUSER="oddnetworks"
